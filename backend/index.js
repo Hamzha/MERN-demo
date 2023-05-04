439678db882a2a2179cf1fa5275e802243dd0486
hamza.ahmed <hamza-ahmed-qbatch>
2023-05-04 12:29:36 +0500

    swagger docs

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,51 @@ require('dotenv').config();
 
 const app = express();
 const PORT = 5000;
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
+// eslint-disable-next-line require-sort/require-sort
 require('./models/number');
 require('./models/user');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'LogRocket Express API with Swagger',
+      version: '0.1.0',
+      description:
+        'This is a simple CRUD API application made with Express and documented with Swagger',
+      license: {
+        name: 'MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      contact: {
+        name: 'LogRocket',
+        url: 'https://logrocket.com',
+        email: 'info@email.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
 
 app.use(cors());
 app.use(express.json());
 app.use(require('./routes/user'));
 
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true }),
+);
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGOURI);
 mongoose.connection.on('connected', () => {
